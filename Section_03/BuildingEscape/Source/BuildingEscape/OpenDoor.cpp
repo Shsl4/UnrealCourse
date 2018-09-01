63876026c4ebcffa9e41b43643b90c4064141cfe
Sl4sh! <42780223+Sl4shZ@users.noreply.github.com>
2018-09-01 15:24:30 -0700

    v1.04

--- Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ Section_03/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "OpenDoor.h"
+#include "Math/Vector.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -19,7 +21,7 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FRotator OwnerRotation = 
 	
 }
 
